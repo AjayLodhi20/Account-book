b3f31acb391e970fefdb91a4014646c7d3077df2
Ajay lodhi <satyamlodhi123@gmail.com>
2026-04-27 15:49:43 +0530

    balance,deposit and withdrawl

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Account:
     current_time = dt.datetime.now(timezone.utc)
 
 
-    def __init__(self, account_no, first_name, last_name, offset, balance):
+    def __init__(self, account_no, first_name, last_name, offset, balance=0):
         self.account_no = account_no
         self._first_name = first_name
         self._last_name = last_name
@@ -34,11 +34,31 @@ class Account:
 
     @property
     def balance(self):
-        return self._balance
+        if self._balance < 0:
+            raise ValueError("Balance should be 0 or higher.")
+        else:
+            return self._balance
+
+    def deposit(self, amount):
+        print(f"{amount} credited ...")
+        self._balance += amount
+
+    def withdraw(self, amount):
+        print(f"{amount} debited...")
+        self._balance -= amount
+
+
+
 
 
 id1 = Account(1234, 'ajay', 'lodhi', 7, 100)
 print(id1.first_name)
 print(id1.full_name)
 print(id1.balance)
+
+id1.deposit(100)
+print(id1.balance)
+id1.withdraw(50)
+print(id1.balance)
+
 
