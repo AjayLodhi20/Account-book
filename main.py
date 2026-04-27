0b72b7f2ca813c9a1a04bac3191a318aaab46262
Ajay lodhi <satyamlodhi123@gmail.com>
2026-04-27 13:03:29 +0530

    define getter and setter

--- main.py
+++ main.py
@@ -9,9 +9,25 @@ class Account:
     def __init__(self, account_no, first_name, last_name, offset, balance):
         self.account_no = account_no
         self._first_name = first_name
-        self.last_name = last_name
+        self._last_name = last_name
         self. tz_offset = offset
         self.balance = balance
     @property
     def first_name(self):
-        self._first_name
+        return self._first_name
+
+    @first_name.setter
+    def first_name(self, value):
+        self._first_name = value
+
+    @property
+    def last_name(self):
+        return self._last_name
+    @last_name.setter
+    def last_name(self, value):
+        self._last_name = value
+
+
+
+id1 = Account(1234, 'ajay', 'lodhi', 7, 100)
+print(id1.first_name)
