3f6ed79f4a052a4df5a83d166dcaafd82eb20f5a
Ajay lodhi <satyamlodhi123@gmail.com>
2026-04-30 12:19:31 +0530

    eivl help

--- main.py
+++ main.py
@@ -5,17 +5,16 @@ import random
 class Account:
     current_time = dt.datetime.now(timezone.utc)
     interest = 0.03
-    transaction_id = 0
-    trans_dict = {}
+    transaction_id = 100
+    result = {}
 
 
-    def __init__(self, account_no, first_name, last_name, offset= 5.5, balance=0):
+    def __init__(self, account_no, first_name, last_name, offset= 7, balance=0):
         self.account_no = account_no
         self._first_name = first_name
         self._last_name = last_name
-        self.tz_offset = offset
+        self.tz_offset =timezone(timedelta(hours = -offset))
         self._balance = balance
-        self.transaction_id = 0
 
 
     @property
@@ -48,25 +47,30 @@ class Account:
     def deposit(self, amount):
         print(f"{amount} credited ...")
         self._balance += amount
-        self.transaction_id += 1
+        Account.transaction_id += 1
+        Account.result['Transaction code'] = 'D'
 
     def withdraw(self, amount):
         if amount > self.balance:
             print("transaction declined...")
-            self.transaction_id += 1
+            Account.transaction_id += 1
         else:
             print(f"{amount} debited...")
             self._balance -= amount
-            self.transaction_id += 1
+            Account.transaction_id += 1
 
     def int_calc(self):
         interest = self._balance * Account.interest
-        self.transaction_id += 1
+        Account.transaction_id += 1
         return interest + self._balance
+    @classmethod
+    def confirmation_number(cls):
+        Account.result['trans_id'] = cls.transaction_id
 
 
-
-
+class Transaction:
+    def __init__(self):
+        self.account_no = account_no
 
 
 
@@ -81,4 +85,9 @@ id1.withdraw(50)
 print(id1.balance)
 
 id1.withdraw(30000)
+print(id1.transaction_id)
 
+satyam = Account(1234, 'satyam', 'lodhi', balance=100)
+satyam.withdraw(105)
+print(satyam.transaction_id)
+print(Account.result)
