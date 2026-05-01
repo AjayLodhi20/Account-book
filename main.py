2d1da4ad8b96ab717705003256b3f15656545359
Ajay lodhi <satyamlodhi123@gmail.com>
2026-05-01 22:04:04 +0530

    final commit

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from datetime import timezone, timedelta
 class Account:
     current_time = dt.datetime.now(timezone.utc)
     interest = 0.03
-    result = {}
 
 
 
@@ -14,8 +13,13 @@ class Account:
         self.account_no = account_no
         self._first_name = first_name
         self._last_name = last_name
-        self.tz_offset =timezone(timedelta(hours = offset))
+        self.tz_offset = offset
         self._balance = balance
+        self.Transaction_code = 'T'
+
+    def confirmation_no(self):
+        result = Transaction(self.account_no,self.Transaction_code, self.tz_offset)
+        return str(result)
 
 
     @property
@@ -40,7 +44,6 @@ class Account:
     @property
     def balance(self):
         if self._balance < 0:
-
             raise ValueError("Balance should be 0 or higher.")
         else:
             return self._balance
@@ -48,17 +51,20 @@ class Account:
     def deposit(self, amount):
         print(f"{amount} credited ...")
         self._balance += amount
-        Account.result['Transaction code'] = 'D'
+        self.Transaction_code = 'D'
 
     def withdraw(self, amount):
         if amount > self.balance:
             print("transaction declined...")
+            self.Transaction_code = 'X'
         else:
             print(f"{amount} debited...")
             self._balance -= amount
+            self.Transaction_code = 'W'
 
     def int_calc(self):
         interest = self._balance * Account.interest
+        self.Transaction_code = 'I'
         return interest + self._balance
 
 class Transaction:
@@ -71,26 +77,20 @@ class Transaction:
         self.time_utc = dt.datetime.now(timezone.utc)
         self.time = self.time_utc.astimezone(timezone(timedelta(hours=tz_offset)))
 
-        def __str__(self):
-            return (f"{self.transaction_code}-{self.account_number}-{self.time_utc.strftime('%Y%m%d%H%M%S')}"
-                    f"-{self.transaction_id}")
-
-
-transact = Transaction(account_number=1, transaction_code='D', tz_offset=-7)
-print(transact)
-conf_no = f"{transact.transaction_code}-{transact.account_number}-{transact.time_utc.strftime('%Y%m$d%H%M%S')}-{transact.transaction_id}"
-print(conf_no)
+    def __str__(self):
+        return (f"{self.transaction_code}-{self.account_number}-{self.time_utc.strftime('%Y%m%d%H%M%S')}"
+                f"-{self.transaction_id}")
 
 
 
-hello = Transaction(1111,100, mst=7)
-print(hello.time)
-print(hello.time_utc)
-hello2 = Account(111112, 'satyam', 'kumar', 7, 203)
-hello2.deposit(200)
-print(hello.transaction_id)
-
-
+hello = Account(11221234, "ajay", "lodhi", 7, 250)
+print(hello.confirmation_no())
+print(hello.deposit(500))
+print(hello.confirmation_no())
+hello.withdraw(250)
+hello.withdraw(250)
+print(hello.balance)
+print(hello.confirmation_no())
 # id1 = Account(1234, 'ajay', 'lodhi', 7, 100)
 # print(id1.first_name)
 # print(id1.full_name)
