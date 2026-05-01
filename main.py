963807531b540f5d0c0ebaa8e524b6f1e30d2227
Ajay lodhi <satyamlodhi123@gmail.com>
2026-05-01 20:49:53 +0530

    eivl help

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import datetime as dt
+import itertools
 from datetime import timezone, timedelta
-import random
+
 
 class Account:
     current_time = dt.datetime.now(timezone.utc)
     interest = 0.03
-    transaction_id = 100
     result = {}
+
 
 
     def __init__(self, account_no, first_name, last_name, offset= 7, balance=0):
         self.account_no = account_no
         self._first_name = first_name
         self._last_name = last_name
-        self.tz_offset =timezone(timedelta(hours = -offset))
+        self.tz_offset =timezone(timedelta(hours = offset))
         self._balance = balance
 
 
@@ -63,31 +64,61 @@ class Account:
         interest = self._balance * Account.interest
         Account.transaction_id += 1
         return interest + self._balance
-    @classmethod
-    def confirmation_number(cls):
-        Account.result['trans_id'] = cls.transaction_id
 
+#
+# class Transaction:
+#     transaction_counter = itertools.count(start =0, step =1)
+#     def __init__(self, account_no, transaction_code, mst):
+#         self.account_no = account_no
+#         self.time_utc = dt.datetime.now(timezone.utc)
+#         self.time = self.time_utc.astimezone(timezone(timedelta(hours= mst)))
+#         self.transaction_id = next(self.transaction_counter)
+#         self.transaction_code = transaction_code
 
 class Transaction:
-    def __init__(self):
-        self.account_no = account_no
+    transaction_counter = itertools.count(start=0, step=1)
+
+    def __init__(self, account_number, transaction_code, tz_offset):
+        self.account_number = account_number
+        self.transaction_code = transaction_code
+        self.transaction_id = next(self.transaction_counter)
+        self.time_utc = dt.datetime.now(timezone.utc)
+        self.time = self.time_utc.astimezone(timezone(timedelta(hours=tz_offset)))
+
+        def __str__(self):
+            return (f"{self.transaction_code}-{self.account_number}-{self.time_utc.strftime('%Y%m%d%H%M%S')}"
+                    f"-{self.transaction_id}")
+
+
+transact = Transaction(account_number=1, transaction_code='D', tz_offset=-7)
+print(transact)
+conf_no = f"{transact.transaction_code}-{transact.account_number}-{transact.time_utc.strftime('%Y%m$d%H%M%S')}-{transact.transaction_id}"
+print(conf_no)
 
 
 
-id1 = Account(1234, 'ajay', 'lodhi', 7, 100)
-print(id1.first_name)
-print(id1.full_name)
-print(id1.balance)
+hello = Transaction(1111,100, mst=7)
+print(hello.time)
+print(hello.time_utc)
+hello2 = Account(111112, 'satyam', 'kumar', 7, 203)
+hello2.deposit(200)
+print(hello.transaction_id)
 
-id1.deposit(100)
-print(id1.balance)
-id1.withdraw(50)
-print(id1.balance)
 
-id1.withdraw(30000)
-print(id1.transaction_id)
-
-satyam = Account(1234, 'satyam', 'lodhi', balance=100)
-satyam.withdraw(105)
-print(satyam.transaction_id)
-print(Account.result)
+# id1 = Account(1234, 'ajay', 'lodhi', 7, 100)
+# print(id1.first_name)
+# print(id1.full_name)
+# print(id1.balance)
+#
+# id1.deposit(100)
+# print(id1.balance)
+# id1.withdraw(50)
+# print(id1.balance)
+#
+# id1.withdraw(30000)
+# print(id1.transaction_id)
+#
+# satyam = Account(1234, 'satyam', 'lodhi', balance=100)
+# satyam.withdraw(105)
+# print(satyam.transaction_id)
+# print(Account.result)
